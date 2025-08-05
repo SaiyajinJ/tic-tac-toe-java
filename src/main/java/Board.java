e1215cc21df3556f94b4d488e255afa0a2b5fed9
Jikar <wi24b101@technikum-wien.at>
2025-08-05 14:07:32 +0200

    project structure (Board, Player, TicTacToe)

---
+++ src/main/java/Board.java
@@ -0,0 +1,23 @@
+public class Board {
+    private char[][] cells;
+
+    public Board() {
+    }
+
+    public boolean isCellEmpty(int x, int y) {
+        return false;
+    }
+
+    public void place(int x, int y, char marker) {
+    }
+
+    public boolean isFull() {
+        return false;
+    }
+
+    public void clear() {
+    }
+
+    public void print() {
+    }
+}
---
+++ src/main/java/TicTacToe.java
@@ -0,0 +1,19 @@
+public class TicTacToe {
+    private Player player1;
+    private Player player2;
+    private Player currentPlayer;
+    private Board board;
+
+    public TicTacToe() {
+    }
+
+    public void start() {
+    }
+
+    private void switchCurrentPlayer() {
+    }
+
+    public boolean hasWinner() {
+        return false;
+    }
+}
